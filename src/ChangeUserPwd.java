f3af7596ee1b64cbbfbbb428dbc0871163ce8178
Aakash-7011 <93108026+Aakash-7011@users.noreply.github.com>
2025-05-06 07:36:49 +0530

    Update ChangeUserPwd.java

--- src/ChangeUserPwd.java
+++ src/ChangeUserPwd.java
@@ -1,4 +1,4 @@
-package com.shashi.servlets;
+package com.aakash.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,12 +11,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.beans.TrainException;
-import com.shashi.beans.UserBean;
-import com.shashi.constant.UserRole;
-import com.shashi.service.UserService;
-import com.shashi.service.impl.UserServiceImpl;
-import com.shashi.utility.TrainUtil;
+import com.aakash.beans.TrainException;
+import com.aakash.beans.UserBean;
+import com.aakash.constant.UserRole;
+import com.aakash.service.UserService;
+import com.aakash.service.impl.UserServiceImpl;
+import com.aakash.utility.TrainUtil;
 
 @SuppressWarnings("serial")
 @WebServlet("/changeuserpwd")
