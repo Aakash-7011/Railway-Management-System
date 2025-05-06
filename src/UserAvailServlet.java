40899c6d0ae4065fcfc5ccb03980afc694ed2fc4
Aakash-7011 <93108026+Aakash-7011@users.noreply.github.com>
2025-05-06 07:42:12 +0530

    Update UserAvailServlet.java

--- src/UserAvailServlet.java
+++ src/UserAvailServlet.java
@@ -1,4 +1,4 @@
-package com.shashi.servlets;
+package com.aakash.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,12 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.beans.TrainBean;
-import com.shashi.beans.TrainException;
-import com.shashi.constant.UserRole;
-import com.shashi.service.TrainService;
-import com.shashi.service.impl.TrainServiceImpl;
-import com.shashi.utility.TrainUtil;
+import com.aakash.beans.TrainBean;
+import com.aakash.beans.TrainException;
+import com.aakash.constant.UserRole;
+import com.aakash.service.TrainService;
+import com.aakash.service.impl.TrainServiceImpl;
+import com.aakash.utility.TrainUtil;
 
 @SuppressWarnings("serial")
 @WebServlet("/useravail")
