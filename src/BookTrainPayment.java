7f407769129600d46b6f0a3ce405a3d6ba2c12ec
Aakash-7011 <93108026+Aakash-7011@users.noreply.github.com>
2025-05-06 07:34:54 +0530

    Update BookTrainPayment.java

--- src/BookTrainPayment.java
+++ src/BookTrainPayment.java
@@ -1,4 +1,4 @@
-package com.shashi.servlets;
+package com.aakash.servlets;
 
 import java.io.IOException;
 
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.constant.UserRole;
-import com.shashi.utility.TrainUtil;
+import com.aakash.constant.UserRole;
+import com.aakash.utility.TrainUtil;
 
 @WebServlet("/payment")
 public class BookTrainPayment extends HttpServlet {
