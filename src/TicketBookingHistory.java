ab2c285b0ae0c43257b7e23237b0fd7b560f346b
Aakash-7011 <93108026+Aakash-7011@users.noreply.github.com>
2025-05-06 07:39:17 +0530

    Update TicketBookingHistory.java

--- src/TicketBookingHistory.java
+++ src/TicketBookingHistory.java
@@ -1,4 +1,4 @@
-package com.shashi.servlets;
+package com.aakash.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,12 +11,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.beans.HistoryBean;
-import com.shashi.beans.TrainException;
-import com.shashi.constant.UserRole;
-import com.shashi.service.BookingService;
-import com.shashi.service.impl.BookingServiceImpl;
-import com.shashi.utility.TrainUtil;
+import com.aakash.beans.HistoryBean;
+import com.aakash.beans.TrainException;
+import com.aakash.constant.UserRole;
+import com.aakash.service.BookingService;
+import com.aakash.service.impl.BookingServiceImpl;
+import com.aakash.utility.TrainUtil;
 
 @SuppressWarnings("serial")
 @WebServlet("/bookingdetails")
