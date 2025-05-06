0da088dac35ff0b51013230af06c453f61bc8e9e
Aakash-7011 <93108026+Aakash-7011@users.noreply.github.com>
2025-05-06 07:37:46 +0530

    Update ErrorHandlerServlet.java

--- src/ErrorHandlerServlet.java
+++ src/ErrorHandlerServlet.java
@@ -1,4 +1,4 @@
-package com.shashi.servlets;
+package com.aakash.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.beans.TrainException;
-import com.shashi.constant.ResponseCode;
+import com.aakash.beans.TrainException;
+import com.aakash.constant.ResponseCode;
 
 public class ErrorHandlerServlet extends HttpServlet {
 
